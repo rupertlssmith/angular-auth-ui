118107aa1e25cda7e48fd12581049ba053f3fc86
Rupert Smith <rupert@thesett.com>
2017-08-23 19:04:12 +0100

    Trimmed down the home page component to just look at authentication.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { ArticleListConfig, TagsService, UserService } from '../shared';
+import { UserService } from '../shared';
 
 @Component({
   selector: 'home-page',
@@ -11,14 +11,10 @@ import { ArticleListConfig, TagsService, UserService } from '../shared';
 export class HomeComponent implements OnInit {
   constructor(
     private router: Router,
-    private tagsService: TagsService,
     private userService: UserService
   ) {}
 
   isAuthenticated: boolean;
-  listConfig: ArticleListConfig = new ArticleListConfig();
-  tags: Array<string> = [];
-  tagsLoaded = false;
 
   ngOnInit() {
     this.userService.isAuthenticated.subscribe(
@@ -27,28 +23,11 @@ export class HomeComponent implements OnInit {
 
         // set the article list accordingly
         if (authenticated) {
-          this.setListTo('feed');
+          console.log("authenticated");
         } else {
-          this.setListTo('all');
+          console.log("not authenticated")
         }
       }
     );
-
-    this.tagsService.getAll()
-    .subscribe(tags => {
-      this.tags = tags;
-      this.tagsLoaded = true;
-    });
-  }
-
-  setListTo(type: string = '', filters: Object = {}) {
-    // If feed is requested but user is not authenticated, redirect to login
-    if (type === 'feed' && !this.isAuthenticated) {
-      this.router.navigateByUrl('/login');
-      return;
-    }
-
-    // Otherwise, set the list object
-    this.listConfig = {type: type, filters: filters};
   }
 }

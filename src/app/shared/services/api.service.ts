1bc62e01545199040202c6b430b7f7ffeb7d5c76
Rupert Smith <rupert@thesett.com>
2017-08-23 19:00:44 +0100

    Set up the api service to inject headers and seperate responses into errors or data.

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -5,14 +5,11 @@ import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-import { JwtService } from './jwt.service';
-
 @Injectable()
 export class ApiService {
   constructor(
-    private http: Http,
-    private jwtService: JwtService
-  ) {}
+    private http: Http
+  ) { }
 
   private setHeaders(): Headers {
     const headersConfig = {
@@ -20,20 +17,17 @@ export class ApiService {
       'Accept': 'application/json'
     };
 
-    if (this.jwtService.getToken()) {
-      headersConfig['Authorization'] = `Token ${this.jwtService.getToken()}`;
-    }
     return new Headers(headersConfig);
   }
 
   private formatErrors(error: any) {
-     return Observable.throw(error.json());
+    return Observable.throw(error.json());
   }
 
   get(path: string, params: URLSearchParams = new URLSearchParams()): Observable<any> {
     return this.http.get(`${environment.api_url}${path}`, { headers: this.setHeaders(), search: params })
-    .catch(this.formatErrors)
-    .map((res: Response) => res.json());
+      .catch(this.formatErrors)
+      .map((res: Response) => res.json());
   }
 
   put(path: string, body: Object = {}): Observable<any> {
@@ -42,8 +36,8 @@ export class ApiService {
       JSON.stringify(body),
       { headers: this.setHeaders() }
     )
-    .catch(this.formatErrors)
-    .map((res: Response) => res.json());
+      .catch(this.formatErrors)
+      .map((res: Response) => res.json());
   }
 
   post(path: string, body: Object = {}): Observable<any> {
@@ -52,8 +46,8 @@ export class ApiService {
       JSON.stringify(body),
       { headers: this.setHeaders() }
     )
-    .catch(this.formatErrors)
-    .map((res: Response) => res.json());
+      .catch(this.formatErrors)
+      .map((res: Response) => res.json());
   }
 
   delete(path): Observable<any> {
@@ -61,7 +55,7 @@ export class ApiService {
       `${environment.api_url}${path}`,
       { headers: this.setHeaders() }
     )
-    .catch(this.formatErrors)
-    .map((res: Response) => res.json());
+      .catch(this.formatErrors)
+      .map((res: Response) => res.json());
   }
 }
